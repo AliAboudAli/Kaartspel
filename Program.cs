328bbe8e7a39e32d4a2ad9d6d17f7f5edf82f934
Ali <aaali2@st.noorderpoort.nl>
2024-06-07 10:29:39 +0200

    update program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace Kaartspel;
 
-class Program
+internal class Program
 {
-   public List<string> kaarten = new List<string> {};
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
+       
         //Maak een kaartspel template met een class of struct voor een kaart en een class voor een deck(een set van een x-aantal kaarten).
         //Zorg voor logische invullingen van beide classes en/of structs.
         //Wanneer een deck geinstantieerd wordt moet het gevuld worden met de juiste 52 kaarten.
@@ -33,25 +33,52 @@ public class Card
 
 public class Deck
 {
-    private List Cards;
+    public List<Card> Cards { get; }
     
-    public struct Card
+    public enum Suits
     {
-        public string suit;
-        public string rank;
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs
     }
+
     public Deck()
     {
-        string[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace" };
+        //ik maak een array van een string aan van alle ranks en jack queen king en aces
+        string[] Ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace" };
         Cards = Cards;
-        public enum suits
+        foreach (var Suits in Enum.GetNames(typeof(Suits)))
         {
-            Hearts,
-            Diamonds,
-            Spades,
-            Clubs
+            foreach (var rank in Ranks)
+            {
+                Cards.Add(new Card(Suits, rank));
+            }
         }
-        
-        
+    }
+
+    public void Shuffle()
+    {
+        Random rnd = new Random();
+        int a = Cards.Count;
+        while (a > 0)
+        {
+            a--;
+            int k = rnd.Next(a + 1);
+            // Wissel de positie van de kaarten op index k en a in het deck
+            Card value = Cards[k];
+            Cards[k] = Cards[a];
+            Cards[a] = value;
+        }
+    }
+
+    public void DrawCard()
+    {
+        if (Cards.Count == 0) throw new InvalidOperationException("No cards left in the deck");
+        {
+            Card drawn = Cards[0];
+            Cards.RemoveAt(0);
+        }
     }
 }
+
